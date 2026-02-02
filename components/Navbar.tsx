e41bb0385c1a77f83b7581de7e6a5ec3f81ab866
alokMIPL <alok@madhulata.com>
2026-02-02 15:43:20 +0530

    code added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav>
       <div>
-        <div className="">
+        <div className="flex justify-between px-32 border-slate-200 border-b-1 bg-slate-100 items-center">
           <div>
             <Link href={"/"}>
               <Image
@@ -16,6 +16,14 @@ const Navbar = () => {
                 height={40}
               />
             </Link>
+          </div>
+          <div>
+            <ul className="flex justify-between gap-10">
+              <li><Link href={"/"}>Browse Jobs</Link></li>
+              <li><Link href={"/"}>Post a Job</Link></li>
+              <li><Link href={"/"}>Dashboard</Link></li>
+              <li><Link href={"/"}>Sign Out</Link></li>
+            </ul>
           </div>
         </div>
       </div>
